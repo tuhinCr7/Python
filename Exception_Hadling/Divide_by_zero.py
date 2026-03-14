71c895984d6f8b6d50d4786b91e4cab37e7f392d
Tuhin Alam Bijoy <tuhinalambijoy@gmail.com>
2026-03-14 12:00:19 +0600

    modifed

--- Exception_Hadling/Divide_by_zero.py
+++ Exception_Hadling/Divide_by_zero.py
@@ -1,10 +1,12 @@
 def calculate(a, b):
     return a/b
+
+
 try:
     a = int(input('Enter first value : '))
     b = int(input('Enter second value : '))
     print(calculate(a, b))
 except ZeroDivisionError:
-    print('Divisior can not be 0 ')  
+    print('Divisior can not be 0 ')
 except ValueError:
-    print('Give numbers only ')    
+    print('Give numbers only ')
--- Exception_Hadling/value_error.py
+++ Exception_Hadling/value_error.py
@@ -1,9 +1,10 @@
 def calculate(a, b):
     return a+b
 
+
 try:
     a = int(input('Enter first value : '))
     b = int(input('Enter second value : '))
     print(calculate(a, b))
 except ValueError:
-    print('Enter numeric only ')    
+    print('Enter numeric only ')
